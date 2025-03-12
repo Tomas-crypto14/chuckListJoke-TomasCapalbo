48c736a305db4af2d6f0362a4a706e6e73ab2ed1
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-03-12 13:20:21 +0100

    Chistes God

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const button = document.getElementById("fetchJoke");
+const lista = document.getElementById("jokelist");
+function chiste(){
+    fetch (`https://api.chucknorris.io/jokes/random`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    const valor = document.createElement("li");
+    const creabutton = document.createElement("button");
+    valor.innerHTML = data.value;
+    creabutton.innerHTML = "Eliminar";
+    creabutton.style.background = "Red";
+    const container = document.getElementById("jokeList");
+    container.appendChild(valor);
+    container.appendChild(creabutton);
+    //Funcion secundaria para borrar el chiste
+    //Como no aqui pide fetch secundario para hacer funciones secundarias, para lo de
+    //eliminar se hace de esa manera
+    //Ese click de addEventListener llama al boton de creabutton
+    creabutton.addEventListener("click", () => {
+        valor.remove();
+        creabutton.remove();
+        localStorage.removeItem("Chiste");
+     });
+    //Local Storage
+    localStorage.setItem("Chiste", data.value);
+    const datos = localStorage.getItem("Chiste");
+    console.log(datos);
+    //storage();
+    })
+}
+button.addEventListener("click", chiste);
+
+/*function storage(){
+    fetch (`https://api.chucknorris.io/jokes/random`)
+    .then((response) => response.json())
+    .then((data) => {
+        localStorage.setItem("Chiste", data.value);
+    })
+}*/
+
+/*function eliminar(){
+    fetch (`https://api.chucknorris.io/jokes/random`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    const valor = document.createElement("li");
+    const creabutton = document.createElement("button");
+    valor.innerHTML = data.value;
+        valor.remove();
+        creabutton.remove();
+        localStorage.removeItem("Chiste");
+    })
+}*/
+//creabutton.addEventListener("click", () => eliminar);
